4f1eb0b8d71e583d564e181f991ee2eb5b2a6f29
Peter Law <PeterJCLaw@gmail.com>
2013-04-12 12:52:46 +0100

    Include team names in the schedule where possible.
    Fall back to TLAs if names aren't available.
    
    Change-Id: Ie0d3ccd086d8647480cfaa9c43455f0845e74750

--- src/display_schedule.py
+++ src/display_schedule.py
@@ -4,6 +4,17 @@ import config
 import talk
 import dateutil.parser
 
+_team_cache = {}
+def get_team(tla):
+    if tla not in _team_cache:
+        team_data = talk.command_yaml('team {0}'.format(tla))
+        #print tla, team_data
+        name = team_data['team']['name']
+        name = name if name is not None else tla
+        _team_cache[tla] = name
+
+    return _team_cache[tla]
+
 config.load_config()
 
 match_data = talk.command('list-matches 2013 2014')
@@ -16,11 +27,14 @@ print '''//TITLE: 2013 Match Schedule
 
 date = None
 
-for line in match_list:
+for line in match_list[1:]:
+    #print line
     dt_str, ident = line.rsplit(':', 1)
     dt = dateutil.parser.parse(dt_str)
     teams_data = talk.command_yaml('get-match-teams {0}'.format(ident))
-    teams = teams_data['teams']
+    #print teams_data
+    team_ids = teams_data['teams']
+    teams = [get_team(t) for t in team_ids]
     this_date = dt.date()
     if date != this_date:
         date = this_date
