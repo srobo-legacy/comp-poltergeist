4a97526c69e2af92749f29e9bd7186bec1e637c6
Peter Law <PeterJCLaw@gmail.com>
2013-04-12 16:54:06 +0100

    Actually sort the league display
    Change-Id: I881c4eb10566002eb08fd96dfab9aee95fb492ca

--- src/display_league.py
+++ src/display_league.py
@@ -1,8 +1,14 @@
 
+from collections import defaultdict
 import config
 
 import talk
 from display_utils import format_name
+
+def gen_pts_tla(points_sorted, points_map):
+    for pts in points_sorted:
+        for tla in points_map[pts]:
+            yield pts, tla
 
 config.load_config()
 
@@ -18,13 +24,19 @@ print '''//TITLE: 2013 Competition League
 | Position | Points | Team |
 |----------|--------|------|'''
 
-pos = 1
+pts_map = defaultdict(lambda: [])
 
-for tla, name in team_list.iteritems():
+for tla in team_list.keys():
     league_data = talk.command_yaml('get-league-points {0}'.format(tla))
     pts = league_data['points']
     pts = pts if pts else 0
-    team = format_name(tla, name)
+    pts_map[pts].append(tla)
+
+pts_list = sorted(pts_map.keys(), reverse=True)
+pos = 1
+
+for pts, tla in gen_pts_tla(pts_list, pts_map):
+    team = format_name(tla, team_list[tla])
 
     if pos == QUALIFYING_TEAMS + 1:
         print '| - | - | - |'
