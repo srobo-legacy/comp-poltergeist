78822e8a95512af166d29c4d0368c2a38008a17a
Peter Law <PeterJCLaw@gmail.com>
2013-04-12 12:29:45 +0100

    Add a test for adding notes to a team.
    Change-Id: I052856296fc19b12beeb8bc950575d6ad7cfba9b

--- src/test_team_db.py
+++ src/test_team_db.py
@@ -107,3 +107,24 @@ def test_get():
                                               'name': 'Name',
                                               'notes': 'Notes',
                                               'present': True})
+
+def test_append_notes():
+    fake_updater = mock.Mock()
+    fake_responder = mock.Mock()
+    fake_values = defer.Deferred()
+    fake_values.callback({'college': 'there',
+                           'name': 'them',
+                           'notes': 'Initial Notes.',
+                           'present': True })
+    fake_getter = mock.Mock(return_value = fake_values)
+    with mock.patch('team_db.roster.get', fake_getter), \
+         mock.patch('team_db.roster.update', fake_updater):
+
+        options = { '<tla>': 'GMR',
+                    '<note>': 'More notes' }
+        team_db.perform_append_notes(fake_responder, options)
+
+        fake_getter.assert_called_once_with('GMR')
+        expected_notes = 'Initial Notes.' + " " + 'More notes'
+        fake_updater.assert_called_once_with('GMR', notes = expected_notes)
+        fake_responder.assert_called_once_with('Team GMR notes updated.')
