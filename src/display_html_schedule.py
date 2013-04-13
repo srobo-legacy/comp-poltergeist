384111677628d01fab43d2365202228d1ae677f5
Peter Law <PeterJCLaw@gmail.com>
2013-04-13 09:38:16 +0100

    Add an html render of the short schedule.
    Change-Id: Ia3cc4ba3dcd34356e8e01caa71fb0b23c8c89d84

---
+++ src/display_html_schedule.py
@@ -0,0 +1,44 @@
+
+import config
+
+import talk
+from datetime import datetime
+from display_utils import get_team_name
+
+config.load_config()
+
+
+match_data = talk.command_yaml('list-matches 2013 2014')
+match_data = match_data['matches']
+
+print '<table><tr>'
+print '<th> Time </th><th> Match </th><th> Zone 0 </th><th> Zone 1 </th><th> Zone 2 </th><th> Zone 3 </th>'
+
+MAX_MATCHES = 10
+
+now = datetime.now()
+date = now.date()
+
+i = 0
+
+for ident, stamp in match_data:
+    print '</tr><tr>'
+    dt = datetime.fromtimestamp(stamp)
+    if dt < now:
+        continue
+    teams_data = talk.command_yaml('get-match-teams {0}'.format(ident))
+
+    team_ids = teams_data['teams']
+    this_date = dt.date()
+    # Don't show the following day
+    if date != this_date:
+        break
+
+    if i >= MAX_MATCHES:
+        break
+    i += 1
+
+    num = ident[6:]
+    print "<td> {0} </td><td> {1} </td><td> {2} </td>".format(dt.time(), num, ' </td><td> '.join(team_ids))
+
+print '</tr></table>'
