d2ee2f9a405859d40c71d80ceeca8e5b40075474
Peter Law <PeterJCLaw@gmail.com>
2013-04-13 10:02:31 +0100

    Fix the score checker so it can actually run.
    Change-Id: I3b54959d7f308ea3360f869981d2769ae4b061ff

--- src/check-input-scores.py
+++ src/check-input-scores.py
@@ -1,21 +1,17 @@
 
-import config
-
-import talk
 from datetime import datetime
+import sys
+
 from display_utils import get_team_name
+import config
+import talk
 
 config.load_config()
 
+MAX_TEAMS_PER_MATCH = 4
+
 match_data = talk.command_yaml('list-matches 2013 2014')
 match_data = match_data['matches']
-
-print '''//TITLE: 2013 Match Schedule
-
-# Match Schedule
-'''
-
-MAX_TEAMS_PER_MATCH = 4
 
 date = None
 fail = False
@@ -23,9 +19,13 @@ fail = False
 for ident, stamp in match_data:
     scores_data = talk.command_yaml('get-scores {0}'.format(ident))
     scores = scores_data['scores']
+    print 'Checking {0}'.format(ident)
+    if scores is None:
+        # No scores for this match - that's fine.
+        continue
     teams = set(scores.keys())
     dsq_data = talk.command_yaml('get-dsqs {0}'.format(ident))
-    dsqs = set(dsq_data['dsqs']
+    dsqs = set(dsq_data['dsqs'])
 
     if len(teams) > MAX_TEAMS_PER_MATCH:
         print >>sys.stderr, "Too many teams in {0}".format(ident)
