522bc69c2aa4648f65d62dc54629541d51a1327b
Peter Law <PeterJCLaw@gmail.com>
2013-04-14 13:19:41 +0100

    Add a script that lists the matches for a given team.
    Change-Id: I909433224671ec2ba0dbaaae5df62437d78a1e93

---
+++ src/matches_for.py
@@ -0,0 +1,25 @@
+
+from datetime import datetime
+import sys
+
+import config
+import talk
+from display_utils import get_team_name, get_delayed_time
+
+config.load_config()
+
+match_data = talk.command_yaml('list-matches 2013-01-01 2014-01-01')
+match_data = match_data['matches']
+
+for ident, stamp in match_data:
+    dt = datetime.fromtimestamp(stamp)
+    dt = get_delayed_time(dt)
+    teams_data = talk.command_yaml('get-match-teams {0}'.format(ident))
+    #print teams_data
+    team_ids = teams_data['teams']
+    if not sys.argv[1] in team_ids:
+        continue
+
+    num = ident[6:]
+#    print "| {0} | {1} | {2} |".format(dt.time(), num, ' | '.join(team_ids))
+    print ident
