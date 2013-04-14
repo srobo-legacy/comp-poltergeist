4cbce270c2bff507996520bb9bea93d137ea284b
Peter Law <PeterJCLaw@gmail.com>
2013-04-14 14:06:51 +0100

    Add a display of the total game points per team.
    Change-Id: I35a30dcec2b45506411b02bb1c9e034081b4e024

---
+++ src/display_game_points.py
@@ -0,0 +1,33 @@
+
+from collections import defaultdict
+
+import config
+import talk
+import display_utils
+
+config.load_config()
+
+QUALIFYING_TEAMS = 24
+
+last_scored_match = display_utils.last_scored_match()
+print '''//TITLE: 2013 Competition Game Points
+
+# Game Points Totals
+
+Up to date with scores from match {0}
+
+| Team | Points |
+|------|--------|'''.format(last_scored_match)
+
+all_scores = display_utils.get_all_match_scores()
+
+scores_map = defaultdict(lambda: 0)
+
+for ident, scores in all_scores.iteritems():
+    for tla, pts in scores.iteritems():
+        scores_map[tla] += pts
+
+all_teams = display_utils.get_team_list()
+for tla in sorted(all_teams.keys()):
+    pts = scores_map[tla]
+    print "| {0} | {1} |".format(tla, pts)
