430527faccef1cb208a378d4b59c7ca7a23858e7
Peter Law <PeterJCLaw@gmail.com>
2013-04-14 11:54:47 +0100

    Add team existance and entrance checking.
    Change-Id: I36807d704c7c3f5f208907c0c423f8148f766ac4

--- src/check-input-scores.py
+++ src/check-input-scores.py
@@ -13,12 +13,17 @@ MAX_TEAMS_PER_MATCH = 4
 match_data = talk.command_yaml('list-matches 2013-01-01 2014-01-01')
 match_data = match_data['matches']
 
+team_data = talk.command_yaml('list-teams')
+all_teams = team_data['list'].keys()
+
 date = None
 fail = False
 
 for ident, stamp in match_data:
     scores_data = talk.command_yaml('get-scores {0}'.format(ident))
     scores = scores_data['scores']
+    teams_data = talk.command_yaml('get-match-teams {0}'.format(ident))
+    team_ids = teams_data['teams']
     print 'Checking {0}'.format(ident)
     if scores is None:
         # No scores for this match - that's fine.
@@ -34,6 +39,15 @@ for ident, stamp in match_data:
     if len(active_teams) > MAX_TEAMS_PER_MATCH:
         print >>sys.stderr, "Too many teams have scores in {0}".format(ident)
         fail = True
+
+    for team in active_teams:
+        if not team in team_ids:
+            print >>sys.stderr, "Non-entrant team {0} have scores in {1}".format(team, ident)
+            fail = True
+
+        if not team in all_teams:
+            print >>sys.stderr, "Non-existent team {0} have scores in {1}".format(team, ident)
+            fail = True
 
 if fail:
     exit(1)
