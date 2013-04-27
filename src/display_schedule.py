1eaedab1af1cb37c2dc9578f91df42c070e737cb
Peter Law <PeterJCLaw@gmail.com>
2013-04-28 00:57:13 +0100

    Add the locations from this year, plus demo CSS.
    Not the right way to store this data, but gets things going
    for now.
    
    Change-Id: I089a16e0605c2d95617f0025b2731e181249960c

--- src/display_schedule.py
+++ src/display_schedule.py
@@ -15,5 +15,7 @@ print '''//TITLE: 2013 Match Schedule
 
 '''
 
-html_schedule = HTMLSchedule(match_data)
+location_data = {'SWI': 'Level 3', 'TWG': 'Concourse', 'EMM': 'Level 2', 'RES': 'Concourse', 'CRB': 'Level 3', 'HRS2': 'Level 3', 'BGR': 'Level 2', 'BPV': 'Level 2', 'SEN2': 'Level 2', 'CPR': 'Level 2', 'BAM': 'Level 2', 'STA': 'Level 3', 'BWS': 'Level 2', 'MFG': 'Level 2', 'JMS': 'Level 3', 'HSO': 'Level 2', 'PSC2': 'Concourse', 'SEN': 'Level 2', 'QMC': 'Concourse', 'HSO2': 'Level 2', 'GRS': 'Level 3', 'CLF': 'Concourse', 'HRS': 'Level 3', 'GRD': 'Concourse', 'spare': 'Pod 1', 'CLY': 'Level 2', 'GMR': 'Pod 1', 'PSC': 'Concourse', 'HZW': 'Level 2', 'PLE': 'Concourse', 'BRK': 'Concourse', 'QEH': 'Concourse', 'MAI': 'Level 2', 'ALT': 'Level 3'}
+
+html_schedule = HTMLSchedule(match_data, location_data)
 print html_schedule.get_all()
